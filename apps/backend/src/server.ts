d7b3689f39b27f4d68a7f0cd5f5253a6c361565c
shaii120 <kofmisar@gmail.com>
2026-03-22 21:38:57 +0200

    server.ts changes when spliting it to smaller components

--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -2,13 +2,9 @@ import express from "express";
 import 'dotenv/config';
 import cors from "cors";
 
-import { PrismaClient } from "@prisma/client";
-import { PrismaMssql } from "@prisma/adapter-mssql";
-import { ReceiptCreateSchema } from "@receipts/shared-schemas";
+import receiptsRoutes from "./routes/receipts.routes.js";
 
 const PORT = process.env.PORT || 3001;
-const adapter = new PrismaMssql(process.env.DATABASE_URL);
-const prisma = new PrismaClient({ adapter });
 const app = express();
 app.use(cors({
   origin: process.env.NEXT_PUBLIC_API_BASE_URL,
@@ -16,21 +12,7 @@ app.use(cors({
 }));
 app.use(express.json());
 
-app.post("/receipts", async (req, res) => {
-  try {
-    const parsedData = ReceiptCreateSchema.parse(req.body);
-    const receipt = await prisma.receipt.create({ data: parsedData });
-    res.json(receipt);
-  } catch (err: Error | any) {
-    console.error(err.message);
-    console.log(err.errors);
-    res.status(400).json({ error: err.message });
-  }
-});
+app.use("/receipts", receiptsRoutes);
 
-app.get("/receipts", async (_req, res) => {
-  const all = await prisma.receipt.findMany();
-  res.json(all);
-});
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
