f83c36e31f84fbf3fcff22d3a7455f1c48d9d988
shaii120 <kofmisar@gmail.com>
2026-04-12 16:18:41 +0300

    Adding some more clearer zod schema error messages

--- apps/packages/prisma-generator-zod-local/src/zod-mapper.ts
+++ apps/packages/prisma-generator-zod-local/src/zod-mapper.ts
@@ -7,8 +7,8 @@ export function fieldsToZodObject(fields: DMMF.Field[], isUpdate: boolean = fals
 }
 
 function fieldToZod(field: DMMF.Field, isUpdate: boolean = false): string {
-  let zodType = prismaScalarToZod(field.type);
   let name = field.name;
+  let zodType = prismaScalarToZod(field.type, name);
 
   if (field.isList) {
     zodType = `z.array(${zodType})`;
@@ -19,17 +19,17 @@ function fieldToZod(field: DMMF.Field, isUpdate: boolean = false): string {
     zodType += '.nullable()';
   }
   else if (field.type == "String") {
-    zodType += ".nonempty()"
+    zodType += `.nonempty(\"${name} is required\")`;
   }
 
   return `  ${name}: ${zodType}`;
 }
 
-function prismaScalarToZod(type: string): string {
+function prismaScalarToZod(type: string, name: string): string {
   switch (type) {
     case 'String': return 'z.string()';
     case 'Int': return 'z.number().int()';
-    case 'Float': return 'z.number()';
+    case 'Float': return `z.number(\"${name} should be a number\")`;
     case 'Boolean': return 'z.boolean()';
     case 'DateTime': return 'z.date()';
     case 'Json': return 'z.unknown()';
